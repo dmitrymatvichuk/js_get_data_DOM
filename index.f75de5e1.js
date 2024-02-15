dc1cc3a648725eddda9f495f0e1603bb9b48a483
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-02-15 10:27:01 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const population = document.querySelectorAll(".population");
+const totalPopulation = document.querySelector(".total-population");
+const averagePopulation = document.querySelector(".average-population");
+const total = [
+    ...population
+].reduce((sum, p)=>{
+    const value = +p.textContent.split(",").join("");
+    return sum + value;
+}, 0);
+const average = (total / population.length).toLocaleString("en-US");
+totalPopulation.textContent = total.toLocaleString("en-US");
+averagePopulation.textContent = average;
+
+//# sourceMappingURL=index.f75de5e1.js.map
